bc7e3abb93d25bc2a5102a8fad7fbc3e3fafd219
milinul <raphalol177@gmail.com>
2025-07-07 16:58:41 -0300

    update

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from .DL_stock_class import StockPredictor  
+from DL_stock_class import StockPredictor  
 
 app = FastAPI(
     title="API de Previsão de Ações",
@@ -44,3 +44,7 @@ async def predict_prices(request: PredictRequest):
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
